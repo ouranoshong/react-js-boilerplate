544f5771f5a191e547f7ad50e2a720a61e3aee63
ouranoshong <ouranoshong@outlook.com>
2017-06-18 17:09:44 +0800

    Update home page style

--- app/pages/home/index.js
+++ app/pages/home/index.js
@@ -1,19 +1,32 @@
 
 import React from 'react';
+import classNames from 'classnames';
+
 import styles from './style.css';
-
 import bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import fontAwesome from 'font-awesome/css/font-awesome.css';
 
+import logo from './logo.svg';
 
 export default class HomePage extends React.Component {
     render() {
         return (
-            <div className={bootstrap.container}>
+            <div>
                 <div className={styles.sbContent}>
                     <h3 className={styles.sbTitle}>React Js Boilerplate</h3>
-                    <p>play with react.</p>
+                    <p className={styles.sbSubTitle}>play with react.</p>
+                    <img src={logo} className={styles.sbLogo} alt="logo" />
+                </div>
+                <div className={bootstrap.container}>
+                    <ul className={classNames(styles.sbInline, styles.sbSocialIntro)}>
+                        <li>
+                            <a href="https://github.com/ouranoshong/react-js-boilerplate" className={classNames(bootstrap.btn, bootstrap['btn-lg'], bootstrap['btn-danger'])}><i className={classNames(fontAwesome.fa, fontAwesome['fa-github'], fontAwesome['fa-fw'])}></i> <span className="network-name">Github</span></a>
+                        </li>
+                    </ul>
                 </div>
             </div>
+
+
         );
     }
 }   
